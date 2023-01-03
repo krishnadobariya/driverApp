ec7304f68c74aa287b76d20a74c02227688acd8e
krishnadobariya <krishnadobariya3488@gmail.com>
2023-01-03 11:18:50 +0530

    ADD:eventAttendees

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -207,6 +207,59 @@ exports.eventList = async (req, res) => {
     }
 }
 
+exports.eventAttendees = async (req, res) => {
+    try {
+
+        let event_id = req.params.id;
+        console.log("event_id::", event_id);
+
+        const findEvent = await joinEvent.find({
+            event_id: event_id
+        });
+        console.log("findEvent::", findEvent);
+
+        const response = [];
+        for (const getUser of findEvent) {
+
+            const findUserDetails = await authModel.findOne({
+                _id: getUser.user_id
+            });
+
+            const usrData = {
+                profile: findUserDetails.profile[0] ? findUserDetails.profile[0].res : "",
+                username: findUserDetails.username,
+                age: findUserDetails.age,
+                gender: findUserDetails.gender
+            }
+
+            response.push(usrData)
+
+        }
+
+        res.status(status.OK).json(
+            {
+                message: "Get All Event's User Details  Successfully",
+                status: true,
+                code: 200,
+                statusCode: 1,
+                data: response
+            }
+        )
+
+    } catch (error) {
+        console.log("userList-Error:", error);
+        res.status(status.INTERNAL_SERVER_ERROR).json(
+            {
+                message: "Something Went Wrong",
+                status: false,
+                code: 500,
+                statusCode: 0,
+                error: error.message
+            }
+        )
+    }
+}
+
 exports.eventDemo = async (req, res) => {
     try {
         const getEventData = await Event.find().sort({ createdAt: -1 })
--- src/routes/event.routes.js
+++ src/routes/event.routes.js
@@ -4,11 +4,13 @@ const upload = require("../utils/multer.photo");
 const {
     addEvent,
     eventList,
+    eventAttendees,
     eventDemo
 } = require("../controllers/event.controller");
 
 router.post("/add/:id", upload.array('profile'), addEvent);
 router.post("/list/:user_id", eventList);
+router.post("/attendees/:id", eventAttendees);
 router.post("/demo", eventDemo);
 
 module.exports = router;
