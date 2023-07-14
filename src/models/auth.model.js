e0f73508a7036650c835d9a17ad5404ee6c135d6
krishnadobariya <krishnadobariya3488@gmail.com>
2023-07-14 16:03:41 +0530

    UPDATE: auth model

--- src/models/auth.model.js
+++ src/models/auth.model.js
@@ -39,7 +39,7 @@ const authSchema = mongoose.Schema({
     status: {
         type: String,
         required: true,
-        default: "Active"
+        default: "Offline"
     },
     user_type: {
         type: Number, //1-Public,2-Private
